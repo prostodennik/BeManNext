c3b9c46e97dc346496b3d91f0f49e341f901f123
Denis Demin <77795430+prostodennik@users.noreply.github.com>
2024-04-09 19:19:48 +0300

    auto-commit

---
+++ src/app/components/Layout.tsx
@@ -0,0 +1,40 @@
+import Head from "next/head";
+
+// import Header from "./Header";
+// import Footer from "./Footer";
+
+interface Props {
+  title: string;
+  keywords: string;
+  description: string;
+  children: React.ReactNode;
+}
+
+export default function Layout({
+  title,
+  keywords,
+  description,
+  children,
+}: Props) {
+  return (
+    <div className="flex flex-col min-h-screen text-primary-color-alt font-roboto">
+      <Head>
+        <title>{title}</title>
+        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {/* <Header /> */}
+      <main className="container mx-auto mt-0 mb-0 sm:mt-7 sm:mb-10">
+        {children}
+      </main>
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: "Russo Trip",
+  description: "Блог о путешествиях по России",
+  keywords: "BeMan",
+};
